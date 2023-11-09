11a2a42a992d178e35bec6577b9c57d05f38124a
22026541-dxtruong <125434377+22026541-dxtruong@users.noreply.github.com>
2023-11-09 12:31:27 +0700

    Add files via upload

---
+++ web_project/app/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from .models import *
+
+# Create your views here.
+def home(request):
+    mydata = story.objects.raw("select * from app_story")
+    return render(request, 'home.html', {'story':mydata})
+
+def list_category(request):
+    mydata = category.objects.raw("select * from app_category")
+    return {"category":mydata}
+
+def search(request):
+    search_story = request.GET['search_story']
+    mydata = story.objects.raw("select * from app_story where name like %s",['%' + search_story + '%'])
+    return render(request, 'search.html', context={'search_story':search_story, "story":mydata})
+    
+    
+def list_story_category(request, id):
+    myCategory = category.objects.raw("select * from app_category where id = %s",[id])
+    myStory = story.objects.raw("select app_story.* from app_category join app_story_categories "
+                               + "on app_category.id = category_id join app_story "
+                               + "on app_story.id = story_id where app_category.id = %s",[id])
+    list_category = category.objects.raw("select * from app_category")
+    return render(request, 'category.html', {'story':myStory, 'category': myCategory, 'list_category': list_category})
+
+def list_chapter(request, id):
+    myChapter = chapters.objects.raw("select * from app_chapters where story_id = %s",[id])
+    myStory = story.objects.raw("select * from app_story where id = %s",[id])
+    myCategory = story.objects.raw("select app_category.* from app_category join app_story_categories "
+                               + "on app_category.id = category_id join app_story "
+                               + "on app_story.id = story_id where app_story.id = %s",[id])
+    return render(request, 'story.html', {'chapters': myChapter, 'story': myStory, 'category': myCategory})
+
+def chapter(request, story_id, id):
+    myStory = story.objects.raw("select * from app_story where id = %s",[story_id])
+    myChapter = chapters.objects.raw("select * from app_chapters where story_id = %s",[story_id])
+    chapter = chapters.objects.raw("select * from app_chapters where story_id = %s and id = %s",[story_id, id])
+    chaptertruoc= chapters.objects.raw("select * from app_chapters where story_id = %s and id = %s",[story_id, id-1])
+    chaptersau = chapters.objects.raw("select * from app_chapters where story_id = %s and id = %s",[story_id, id+1])
+    return render(request, 'chapter.html', {'chapter': chapter, 'chaptertruoc': chaptertruoc, 'chaptersau': chaptersau, 'story': myStory, 'list_chapter': myChapter})
