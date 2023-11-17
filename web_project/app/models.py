3ab22543452860b5eaf70cde596c3254d5be20cd
22026541-dxtruong <125434377+22026541-dxtruong@users.noreply.github.com>
2023-11-17 14:31:29 +0700

    Add files via upload

---
+++ web_project/app/models.py
@@ -0,0 +1,83 @@
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import pre_delete, post_save
+from django.dispatch import receiver
+from mptt.models import MPTTModel, TreeForeignKey
+
+# Create your models here.
+class author(models.Model):
+    name = models.CharField(default="", max_length=50, blank=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+class category(models.Model):
+    name = models.CharField(default="", max_length=50)
+    content = models.TextField(default="", max_length=1000)
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+class story(models.Model):
+    CHOICES = (
+        ("HT", "Hoàn thành"),
+        ("DCN", "Đang cập nhật"),
+    )
+    image = models.ImageField(upload_to="app/image")
+    name = models.CharField(default="", max_length=255)
+    authors = models.ManyToManyField(author, blank=True)
+    status = models.CharField(choices=CHOICES, max_length=255,default='')
+    date =  models.DateField(auto_now_add=True)
+    alias = models.CharField(default="", max_length=255)
+    content = models.TextField(default="", max_length=1000)
+    categories = models.ManyToManyField(category,blank=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+@receiver(pre_delete, sender=story)
+def remove_file(**kwargs):
+    instance = kwargs.get('instance')
+    instance.file.delete(save=False)
+
+   
+class chapters(models.Model):
+    name = models.CharField(default="", max_length= 50)
+    date =  models.DateField(auto_now_add=True)
+    file = models.FileField(upload_to="app/file")
+    story = models.ForeignKey(story,blank=True,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.name} {self.file} {self.date}"
+    
+@receiver(pre_delete, sender=chapters)
+def remove_file(**kwargs):
+    instance = kwargs.get('instance')
+    instance.file.delete(save=False)
+    
+    
+class UserStory(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    story = models.ForeignKey(story, on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+@receiver(post_save, sender=chapters)
+def send_update_notification(sender, instance, created, **kwargs):
+    if created:
+        user_stories = UserStory.objects.filter(story=instance.story)
+        for user_story in user_stories:
+            user_story.read = False
+            user_story.save()
+    
+class comment(MPTTModel):
+    content = models.CharField(default='', max_length=255,null=True,blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    chapters = models.ForeignKey(chapters,blank=True,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.content} {self.date}"
+    class MPTTMeta:
+        order_insertion_by = ['content']
---
+++ web_project/app/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import *
+
+# Create your views here.
+def home(request):
+    mydata = story.objects.raw("select * from app_story")
+    return render(request, 'app/home.html', {'story':mydata})
+
+def list_category(request):
+    mydata = category.objects.raw("select * from app_category")
+    myStatus = story.objects.raw("select distinct(status) from app_story")
+    user_stories = UserStory.objects.filter(user=request.user).order_by('-date')
+    if request.method == "POST":
+        story_id = request.POST.get("hidden")
+        notification = get_object_or_404(UserStory, id=int(story_id))
+        if request.user == notification.user:
+            notification.read = True
+            notification.save()
+        return redirect("/story/" + str(story_id))
+    return {"category":mydata, "status": myStatus,"notifications": user_stories,}
+
+def search(request):
+    search_story = request.GET['search_story']
+    mydata = story.objects.raw("select * from app_story where name like %s",['%' + search_story + '%'])
+    return render(request, 'app/search.html', context={'search_story':search_story, "story":mydata})
+    
+    
+def list_story_category(request, id):
+    myCategory = category.objects.raw("select * from app_category where id = %s",[id])
+    myStory = story.objects.raw("select app_story.* from app_category join app_story_categories "
+                               + "on app_category.id = category_id join app_story "
+                               + "on app_story.id = story_id where app_category.id = %s",[id])
+    list_category = category.objects.raw("select * from app_category")
+    return render(request, 'app/category.html', {'story':myStory, 'category': myCategory, 'list_category': list_category})
+
+def list_chapter(request, id):
+    myChapter = chapters.objects.raw("select * from app_chapters where story_id = %s",[id])
+    myStory = story.objects.raw("select * from app_story where id = %s",[id])
+    myCategory = story.objects.raw("select app_category.* from app_category join app_story_categories "
+                               + "on app_category.id = category_id join app_story "
+                               + "on app_story.id = story_id where app_story.id = %s",[id])
+    user_story = get_object_or_404(story, id=id)
+    if request.method == "POST":
+        if 'unfollow' in request.POST:
+            UserStory.objects.filter(user=request.user, story=user_story).delete()
+        elif 'follow' in request.POST:
+            UserStory.objects.create(user=request.user, story=user_story).save()
+        return redirect("/story/" + str(id))
+    
+    is_following = UserStory.objects.filter(user=request.user, story=user_story).exists()
+        
+    return render(request, 'app/story.html', {'chapters': myChapter, 'story': myStory, 'category': myCategory, 'is_following': is_following})
+def chapter(request, story_id, chapter_id):
+    myStory = story.objects.raw("select * from app_story where id = %s",[story_id])
+    myChapter = chapters.objects.raw("select * from app_chapters where story_id = %s",[story_id])
+    chapter = chapters.objects.raw("select * from app_chapters where story_id = %s and id = %s",[story_id, chapter_id])
+    chaptertruoc= chapters.objects.raw("select * from app_chapters where story_id = %s and id = %s",[story_id, chapter_id-1])
+    chaptersau = chapters.objects.raw("select * from app_chapters where story_id = %s and id = %s",[story_id, chapter_id+1])
+    
+    if request.method == "POST":
+        comment_content = request.POST.get('comment')
+        reply_content = request.POST.get('reply')
+        if 'delete' in request.POST:
+            get_object_or_404(comment, id=int(request.POST.get('delete'))).delete()
+        if comment_content and comment_content.strip() != '':
+            addComment = comment.objects.create(content=comment_content,user=request.user,chapters_id=chapter_id)
+            addComment.save()
+        if reply_content and reply_content.strip() != '':
+            parent_comment = get_object_or_404(comment, id=int(request.POST.get('hidden')))
+            addReply = comment.objects.create(content=reply_content,user=request.user,chapters_id=chapter_id,parent= parent_comment)
+            addReply.save()
+        return redirect("/story/" + str(story_id) + "/chapter/" + str(chapter_id))
+            
+    myComment = comment.objects.all()
+        
+    file = chapter[0].file.open('r')
+    return render(request, 'app/chapter.html', {'chapter': chapter, 'chaptertruoc': chaptertruoc, 'chaptersau': chaptersau, 
+                                            'story': myStory, 'list_chapter': myChapter, 'data': file.read(), 'list_comment': myComment,
+                                            })
+    
