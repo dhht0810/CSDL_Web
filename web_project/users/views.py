2e1e40504cf802090a3dc5c71add825ad6236258
22026541-dxtruong <125434377+22026541-dxtruong@users.noreply.github.com>
2023-11-18 19:58:56 +0700

    Add files via upload

---
+++ web_project/users/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import redirect, render
+from .models import *
+from .forms import *
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+
+def register(request):
+    form = CustomUserCreationForm()
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, "users/register.html", {'form': form, })    
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect("/")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
+        
+def logout(request):
+    auth_logout(request)
+    return redirect("/")
+
+
