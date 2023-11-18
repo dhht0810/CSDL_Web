96be87cb8f6fe603351e3de0a5269618e7664bd7
22026541-dxtruong <125434377+22026541-dxtruong@users.noreply.github.com>
2023-11-18 18:10:41 +0700

    Delete web_project directory

--- web_project/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("signup/", register, name="register"),
-    path("login/",login, name="login"),
-    path("logout/",logout, name='logout'),
-]
